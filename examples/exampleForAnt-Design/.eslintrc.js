48be4c583159cb5dc15aea5496508dd023ccc72c
DOCDOCde <1324121459@qq.com>
2017-07-12 23:53:42 +0800

    update

--- examples/exampleForAnt-Design/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by DOCer 17/7/11.
- */
-module.exports = {
-    "extends": "Standard",
-};
--- examples/exampleForAnt-Design/src/Root/modules/Test/models/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by DOCer on 2017/7/11.
- */
-import test from './test';
-
-export default [test];
